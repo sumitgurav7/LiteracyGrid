e9d5e3d12c15b02c03304a5a516800490d50a078
Sumit Kiran Gurav <SumitKiran.Gurav@euromonitor.com>
2020-01-28 15:20:46 +0530

    Svg Chart creation

--- src/app/literacy-chart/literacy-chart.component.ts
+++ src/app/literacy-chart/literacy-chart.component.ts
@@ -24,13 +24,74 @@ export class LiteracyChartComponent implements OnInit {
   }
 
 
+  // private populateChart() {
+  //   d3.select('.chart')
+  //     .selectAll('div')
+  //     .data(this.rowData)
+  //     .enter().append('div')
+  //     .style('width', (data) => data.literacyRatePersons + 'px')
+  //     .style('height', '25px').style('background-color', 'blue').style('margin', '5px')
+  //     .text((data) => data.nameOfStatesUts);
+  // }
+
   private populateChart() {
-    d3.select('.chart')
-      .selectAll('div')
+    const svg = this.createSvg();
+    const tooltip = this.createToolTip();
+    svg.attr('width', 500).attr('height', 600);
+    svg.selectAll('rect')
       .data(this.rowData)
-      .enter().append('div')
-      .style('width', (data) => data.literacyRatePersons + 'px')
-      .style('height', '25px').style('background-color', 'blue').style('margin', '5px')
-      .text((data) => data.nameOfStatesUts);
+      .enter()
+        .append('rect')
+        .attr('x', (d, i) => {
+          return 0; })
+        .attr('y', (d, i) => {
+          const len = (i * 20) ;
+          console.log(`index = ${i} , len = ${len}`);
+          return len; })
+        .attr('width', (d, i) => {
+          const len = d.literacyRatePersons + '%';
+          return len; })
+        .attr('height', '10px')
+        .attr('fill', 'red')
+        .append('title')
+        .text((d) => 'literate' + (d.literacyRatePersons) + ' %   ')
+      .exit()
+      .data(this.rowData)
+      .enter()
+        .append('rect')
+        .attr('x', (d, i) => {
+          const len = (d.literacyRatePersons * 5) ;
+          return len; })
+        .attr('y', (d, i) => {
+            const len = (i * 20) ;
+            console.log(`index = ${i} , len = ${len}`);
+            return len; })
+        .attr('width', (d, i) => {
+            const len = (100 - d.literacyRatePersons) + '%';
+            return len; })
+        .attr('height', '10px')
+        .attr('fill', 'blue')
+        .append('title')
+        .text((d) => 'illiterate' + (100 - d.literacyRatePersons) + ' %   ');
+        // .on('mouseover', (d) => {
+        //     tooltip.text('illiterate' + (100 - d.literacyRatePersons) + ' %   ');
+        //     return tooltip.style('visibility', 'visible'); })
+        // .on('mousemove', () => {
+        //   return tooltip.style('top', (d3.event.pageY - 10) + 'px').style('left', (d3.event.pageX + 10) + 'px'); 
+        // })
+        // .on('mouseout', () => tooltip.style('visibility', 'hidden' ));
+  }
+  createToolTip() {
+    return d3.select('.chart')
+    .append('div')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('visibility', 'hidden')
+    .style('background', 'yellow')
+    .text('a simple tooltip');
+  }
+  createSvg() {
+    return d3.select('.chart')
+      .append('svg');
   }
 }
